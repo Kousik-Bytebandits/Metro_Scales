52daf62237f51d6031ccbe47b77439ebdac8ba3a
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-04-29 18:51:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ height: '100vh' }}>
+      <iframe
+        src="/home.html"
+        title="Static Home Page"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+      />
+    </div>
+  );
+}
+
+export default App;
